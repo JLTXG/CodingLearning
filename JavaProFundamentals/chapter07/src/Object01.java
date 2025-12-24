d65250ddfa4ad23077037a6933031720f4ab0147
leiweijun <136902679+JLTXG@users.noreply.github.com>
2025-12-24 20:05:33 +0800

    类与对象的引出

---
+++ JavaProFundamentals/chapter07/src/Object01.java
@@ -0,0 +1,23 @@
+/**
+ * @author AJun
+ * @createDate 2025/12/24 21:22
+ * @description:
+ */
+public class Object01 {
+
+    public static void main(String[] arghs) {
+
+        String cat1Name = "小白";
+        int cat1Age = 1;
+        String cat1Color = "白色";
+
+        String cat2Name = "小黑";
+        int cat2Age = 2;
+        String cat2Color = "黑色";
+
+        String[] cat1 = {"小白", "3", "白色"};
+        String[] cat2 = {"小黑", "4", "黑色"};
+
+    }
+
+}
